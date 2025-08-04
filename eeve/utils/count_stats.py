c34dc77fb9fbc743c67d17b29e60cdf144eb0778
whatisslove11 <whatisslove7@gmail.com>
2025-08-04 18:50:51 +0300

    add hydra configs & baseline embs training

---
+++ eeve/utils/count_stats.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer
+
+
+class CountStats:
+    """
+    Считает статистики по токенам (среднюю длину токена, количество UNK и прочее)
+    """
+    def __init__(self, tokenizer, dataset):
+        pass
+
+    def _count_stats(self):
+        pass
---
+++ eeve/utils/preprocess.py
@@ -0,0 +1,9 @@
+def get_detailed_instruct_for_one_sample(task_description: str, query: str) -> str:
+    return f'Instruct: {task_description}\nQuery: {query}'
+
+
+def get_detailed_instruct_for_ds_example(example: dict, task_description: str) -> str:
+    return {
+        k: get_detailed_instruct_for_one_sample(query=v, task_description=task_description)
+        for k, v in example.items()
+    }
