198815d526ef84d286e49d9578c025648266e067
whatisslove11 <whatisslove7@gmail.com>
2025-08-05 16:09:51 +0300

    fix setnence transformers trainer & hydra config bugs

--- eeve/data/align_pairs/train_emb.py
+++ eeve/data/align_pairs/train_emb.py
@@ -1,5 +1,6 @@
 import os
 import hydra
+import OmegaConf
 from omegaconf import DictConfig
 
 import torch
@@ -7,59 +8,75 @@ import sentence_transformers
 from sentence_transformers import SentenceTransformerTrainer
 from eeve.utils.datasets import _load_dataset_from_path
 
-@hydra.main(config_path="/content/config", config_name="colab", version_base=None)
+
+@hydra.main(config_path="../../training_configs", config_name="st_hydra_config", version_base=None)
 def train(cfg: DictConfig):
     train_dataset = None
     eval_dataset = None
+    load_kwargs = OmegaConf.to_container(
+        cfg.data.train.get('load_kwargs', {}), 
+        resolve=True
+    )
 
     if test_size_from_train := cfg.data.eval.get('test_size_from_train'):
         ds_dict = _load_dataset_from_path(
             path=cfg.data.train.path,
             test_size=test_size_from_train,
+            load_kwargs=load_kwargs
         )
         train_dataset = ds_dict['train']
         eval_dataset = ds_dict['test']
 
     elif split_from_train_path := cfg.data.eval.get('split_from_train_path'):
-        ds_dict = _load_dataset_from_path(path=cfg.data.train.path)
+        ds_dict = _load_dataset_from_path(
+            path=cfg.data.train.path,
+            load_kwargs=load_kwargs
+        )
         train_dataset = ds_dict[cfg.data.train.split]
         eval_dataset = ds_dict[split_from_train_path]
 
     elif cfg.data.eval.get('path'):
-        train_ds_dict = _load_dataset_from_path(path=cfg.data.train.path)
-        train_dataset = train_ds_dict[cfg.data.train.split]
-        
+        train_ds_dict = _load_dataset_from_path(path=cfg.data.train.path, load_kwargs=load_kwargs)
         eval_ds_dict = _load_dataset_from_path(path=cfg.data.eval.path)
+        
+        train_dataset = train_ds_dict[cfg.data.train.split]
         eval_dataset = eval_ds_dict[cfg.data.eval.get('split', 'train')]
 
     if train_dataset is None or eval_dataset is None:
         raise ValueError("Не удалось определить датасеты для обучения и оценки. "
                          "Проверьте конфигурацию в `data.eval`.")
-        
-    eval_source_sentences = list(eval_dataset[cfg.data.source_column])
-    eval_target_sentences = list(eval_dataset[cfg.data.target_column])
 
     if cfg.preprocessing.get('apply_function'):
         preprocess_func = hydra.utils.get_method(cfg.preprocessing.apply_function._target_)
-        fn_kwargs = cfg.preprocessing.apply_function.get('fn_kwargs', {})
+        fn_kwargs = OmegaConf.to_container(
+            cfg.preprocessing.apply_function.get('fn_kwargs', {}), 
+            resolve=True
+        )
 
         train_dataset = train_dataset.map(
             lambda example: preprocess_func(example=example, **fn_kwargs),
-            load_from_cache_file=False
+            load_from_cache_file=True
         )
         eval_dataset = eval_dataset.map(
             lambda example: preprocess_func(example=example, **fn_kwargs),
-            load_from_cache_file=False
+            load_from_cache_file=True
         )
-
 
     model = hydra.utils.instantiate(cfg.model)
     loss = hydra.utils.instantiate(cfg.loss, model=model)
+
+    source_sentences = list(eval_dataset[cfg.data.source_column])
+    target_sentences = list(eval_dataset[cfg.data.target_column])
+
+    # TODO: допилить для использования любых параметров в evaluator
     evaluator = hydra.utils.instantiate(
         cfg.evaluator,
-        source_sentences=eval_source_sentences,
-        target_sentences=eval_target_sentences,
+        source_sentences=list(source_sentences),
+        target_sentences=target_sentences,
     )
+
+    print(evaluator.name)
+    print(type(evaluator.source_sentences))
 
     training_args = hydra.utils.instantiate(cfg.training)
     trainer = SentenceTransformerTrainer(
--- eeve/utils/preprocess.py
+++ eeve/utils/preprocess.py
@@ -2,8 +2,8 @@ def get_detailed_instruct_for_one_sample(task_description: str, query: str) -> s
     return f'Instruct: {task_description}\nQuery: {query}'
 
 
-def get_detailed_instruct_for_ds_example(example: dict, task_description: str) -> str:
-    return {
-        k: get_detailed_instruct_for_one_sample(query=v, task_description=task_description)
-        for k, v in example.items()
-    }
+# def get_detailed_instruct_for_ds_example(example: dict, task_description: str) -> str:
+#     return {
+#         k: get_detailed_instruct_for_one_sample(query=v, task_description=task_description)
+#         for k, v in example.items()
+#     }
