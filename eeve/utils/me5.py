a99f199effba79dd090a372afe8ff26d3d95a976
whatisslove11 <whatisslove7@gmail.com>
2025-09-20 18:38:43 +0300

    small refactor

--- eeve/utils/me5.py
+++
@@ -1,6 +0,0 @@
-def get_detailed_instruct_for_ds_example(example: dict, task_description: str, source_column, target_column) -> dict:
-    return {
-        source_column: f'Instruct: {task_description}\nQuery: {example[source_column]}',
-        # target_column: f'Instruct: {task_description}\nQuery: {example[target_column]}'
-        target_column: example[target_column]
-    }
